70ef805f97668c1f6822dc7ded5cf3e618c5d001
duyuqian <15738381799@163.com>
2020-01-06 12:38:12 +0800

    [duyuqian] feat-4: complete the Student class and the Worker class ,to change the introduce

--- main/practice_4/student.js
+++ main/practice_4/student.js
@@ -1,1 +1,13 @@
+import Person from "../../main/practice_4/person.js";
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 
+    introduce() {
+        return super.basic_introduce() + ' I am a Student. I am at Class ' + this.klass + '.';
+    }
+}
+
+module.exports = Student;
