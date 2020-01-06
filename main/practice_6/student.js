60243570a91471564bb6a66347e87fb29786f13e
duyuqian <15738381799@163.com>
2020-01-06 14:36:16 +0800

    [duyuqian] feat-6: complete the Student class and the Teacher class use Person introduce

--- main/practice_6/student.js
+++ main/practice_6/student.js
@@ -1,1 +1,12 @@
+import Person from "../../main/practice_6/person.js";
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
 
+    introduce() {
+        return super.introduce() + ' I am a Student. I am at Class ' + this.klass + '.';
+    }
+}
+module.exports = Student;
