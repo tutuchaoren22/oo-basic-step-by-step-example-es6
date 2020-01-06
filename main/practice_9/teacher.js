651c20326814c5d4b118355e8b8d7c42d0b7e387
duyuqian <15738381799@163.com>
2020-01-06 18:12:36 +0800

    [duyuqian] feat-9: complete the Student ,the Class and the Teacher class with id,and add the appendMember function

--- main/practice_9/teacher.js
+++ main/practice_9/teacher.js
@@ -1,1 +1,24 @@
+import Person from "../../main/practice_9/person.js";
+class Teacher extends Person {
+    constructor(id, name, age, klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
 
+    introduce() {
+        if (this.klass) {
+            return super.introduce() + ' I am a Teacher. I teach ' + this.klass.getDisplayName() + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I teach No Class.';
+        }
+    }
+
+    introduceWith(student) {
+        if (student.klass === this.klass) {
+            return super.introduce() + ' I am a Teacher. I teach ' + student.name + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I don\'t teach ' + student.name + '.';
+        }
+    }
+}
+module.exports = Teacher;
