a8353e2add5e11579456334660586610780d8038
duyuqian <15738381799@163.com>
2020-01-06 18:44:55 +0800

    [duyuqian] feat-10: complete the Student ,the Class and the Teacher class with id,and add the isIn and isTeaching function

--- main/practice_10/class.js
+++ main/practice_10/class.js
@@ -1,1 +1,22 @@
-
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+    getDisplayName() {
+        return 'Class ' + this.number;
+    }
+    assignLeader(student) {
+        if (student.klass === this) {
+            this.leader = student;
+        } else {
+            console.log('It is not one of us.');
+        }
+    }
+    appendMember(student) {
+        student.klass = this;
+    }
+    isIn(student) {
+        return student.klass === this;
+    }
+}
+module.exports = Class;
--- main/practice_10/teacher.js
+++ main/practice_10/teacher.js
@@ -1,1 +1,36 @@
+import Person from "../../main/practice_10/person.js";
 
+class Teacher extends Person {
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce() {
+        if (this.klasses) {
+            const klasses = this.klasses.reduce(function(previousValue, currentValue) {
+                return previousValue.number + ', ' + currentValue.number;
+            });
+            return super.introduce() + ' I am a Teacher. I teach Class ' + klasses + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I teach No Class.';
+        }
+    }
+
+    introduceWith(student) {
+        if (student.klass in this.klasses) {
+            return super.introduce() + ' I am a Teacher. I teach ' + student.name + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I don\'t teach ' + student.name + '.';
+        }
+    }
+    isTeaching(student) {
+        for (let i = 0; i < this.klasses.length; i++) {
+            if (this.klasses[i].isIn(student)) {
+                return true;
+                break;
+            }
+        }
+    }
+}
+module.exports = Teacher;
