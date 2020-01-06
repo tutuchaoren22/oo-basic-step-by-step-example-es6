fcdb4243dcbb018267d2db510c0d3354dc7edbb7
duyuqian <15738381799@163.com>
2020-01-06 19:31:45 +0800

    [duyuqian] feat-11: complete the Student ,the Class and the Teacher class,and add the registerAssignLeaderListener and registerJoinListener function

--- main/practice_11/class.js
+++ main/practice_11/class.js
@@ -1,1 +1,37 @@
+class Class {
+    constructor(number) {
+        this.number = number;
+    }
+    getDisplayName() {
+        return 'Class ' + this.number;
+    }
+    assignLeader(student) {
+        if (student.klass === this) {
+            this.leader = student;
+            if (student.klass.assignLeaderListener) {
+                console.log('I am ' + student.klass.assignLeaderListener.name + '. I know ' +
+                    student.name + ' become Leader of ' + student.klass.getDisplayName() + '.');
+            }
 
+        } else {
+            console.log('It is not one of us.');
+        }
+    }
+    appendMember(student) {
+        student.klass = this;
+        if (student.klass.joinListener) {
+            console.log('I am ' + student.klass.joinListener.name + '. I know ' +
+                student.name + ' has joined ' + student.klass.getDisplayName() + '.');
+        }
+    }
+    isIn(student) {
+        return student.klass === this;
+    }
+    registerAssignLeaderListener(teacher) {
+        this.assignLeaderListener = teacher;
+    }
+    registerJoinListener(teacher) {
+        this.joinListener = teacher;
+    }
+}
+module.exports = Class;
--- main/practice_11/teacher.js
+++ main/practice_11/teacher.js
@@ -1,1 +1,44 @@
+import Person from "../../main/practice_10/person.js";
 
+class Teacher extends Person {
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce() {
+        if (this.klasses) {
+            const klasses = this.klasses.reduce(function(previousValue, currentValue) {
+                return previousValue.number + ', ' + currentValue.number;
+            });
+            return super.introduce() + ' I am a Teacher. I teach Class ' + klasses + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I teach No Class.';
+        }
+    }
+
+    introduceWith(student) {
+        if (student.klass in this.klasses) {
+            return super.introduce() + ' I am a Teacher. I teach ' + student.name + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I don\'t teach ' + student.name + '.';
+        }
+    }
+    isTeaching(student) {
+        for (let i = 0; i < this.klasses.length; i++) {
+            if (this.klasses[i].isIn(student)) {
+                return true;
+                break;
+            }
+        }
+    }
+
+}
+module.exports = Teacher;
+// 当学生加入Teacher教的班级的时候，Teacher会打印一个句话，形如：
+
+// >I am Tom. I know Jerry has joined Class 2.
+
+// 当学生成为Teacher教的班级的班长的时候，Teacher会打印一句话，形如：
+
+// >I am Tom. I know Jerry become Leader of Class 2.
