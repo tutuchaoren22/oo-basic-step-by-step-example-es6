d02adfa462b8f0efe889f32a56c2ce737c079375
duyuqian <15738381799@163.com>
2020-01-06 14:27:55 +0800

    [duyuqian] feat-5: complete the Student class and the Worker class use Person introduce

--- main/practice_5/worker.js
+++ main/practice_5/worker.js
@@ -1,1 +1,12 @@
+import Person from "../../main/practice_5/person.js";
+class Worker extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
 
+    introduce() {
+        return super.introduce() + ' I am a Teacher. I have a job.';
+    }
+}
+
+module.exports = Worker;
