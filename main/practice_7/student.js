03b9e8cebf4ea92c5c028974ea7fbb8d5c363653
duyuqian <15738381799@163.com>
2020-01-06 17:24:21 +0800

    [duyuqian] feat-7: complete the Student ,the Class and the Teacher class

--- main/practice_7/student.js
+++ main/practice_7/student.js
@@ -1,1 +1,13 @@
+import Person from "../../main/practice_7/person.js";
 
+class Student extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        return super.introduce() + ' I am a Student. I am at ' + this.klass.getDisplayName() + '.';
+    }
+}
+module.exports = Student;
--- main/practice_7/teacher.js
+++ main/practice_7/teacher.js
@@ -1,1 +1,25 @@
+import Person from "../../main/practice_7/person.js";
 
+class Teacher extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce() {
+        if (this.klass) {
+            return super.introduce() + ' I am a Teacher. I teach ' + this.klass.getDisplayName() + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I teach No Class.';
+        }
+    }
+
+    introduceWith(student) {
+        if (student.klass === this.klass) {
+            return super.introduce() + ' I am a Teacher. I teach ' + student.name + '.';
+        } else {
+            return super.introduce() + ' I am a Teacher. I don\'t teach ' + student.name + '.';
+        }
+    }
+}
+module.exports = Teacher;
